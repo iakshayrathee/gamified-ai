3b388d3e35365c67e844d7bfa2e3453823785cd2
iakshayrathee <akshayrathee902@gmail.com>
2025-12-31 15:28:15 +0530

    feat: downgrade to prisma 6.10.0 from v7

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -4,7 +4,6 @@ const getPrismaClient = () => {
   return new PrismaClient({
     errorFormat: process.env.NODE_ENV === 'production' ? 'minimal' : 'colorless',
     log: [], // empty array avoids logging
-     adapter: 'binary',
   });
 };
 
