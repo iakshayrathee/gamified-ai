31fd4bff6abf00579c25dd258f3acfa55e8632d3
Akshay Rathee <96234377+iakshayrathee@users.noreply.github.com>
2025-12-31 15:24:03 +0530

    added adapter to db.ts

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -4,6 +4,7 @@ const getPrismaClient = () => {
   return new PrismaClient({
     errorFormat: process.env.NODE_ENV === 'production' ? 'minimal' : 'colorless',
     log: [], // empty array avoids logging
+     adapter: 'binary',
   });
 };
 
